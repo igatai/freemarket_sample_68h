3b737267c7848b98bd79b76e5758689a56589c42
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-02-14 16:17:53 +0900

    add content column

--- db/migrate/20200213084242_create_products.rb
+++ db/migrate/20200213084242_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :name, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
