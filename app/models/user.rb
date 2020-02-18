76220e2433a3017ed367a8c6a17d4ca53903982b
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-02-18 17:51:50 +0900

    create login signup routes
    新規登録、loginのリンク完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :family_name,           presence: true
+  validates :first_name,            presence: true
+  validates :family_name_zenkaku,  presence: true
+  validates :first_name_zenkaku,   presence: true
+  validates :nickname,              presence: true
+  validates :birthyear,             presence: true
+  validates :birthmonth,            presence: true
+  validates :birthday,              presence: true
+  validates :phone_number,          presence: true
+
 end
