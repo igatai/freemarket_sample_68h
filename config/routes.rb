b49f47c404136f377e9577c7ba811e8d4fbe91e3
MasahiroTagashira <tagashira0624@gmail.com>
2020-02-18 16:00:50 +0900

    exchange_routes_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'products#show'
+  root 'products#index'
   resources :useraddress, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
