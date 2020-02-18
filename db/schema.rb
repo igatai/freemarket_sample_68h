06f8102a03704d9a3e59f65c1a9d1d6046ff3962
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-02-18 16:38:30 +0900

    migrate comp
    usersテーブルのマイグレーションが完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_082129) do
+ActiveRecord::Schema.define(version: 2020_02_18_064902) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,6 +27,23 @@ ActiveRecord::Schema.define(version: 2020_02_14_082129) do
     t.integer "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "birthyear", null: false
+    t.string "birthmonth", null: false
+    t.string "birthday", null: false
+    t.string "family_name_zenkaku", null: false
+    t.string "first_name_zenkaku", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "phone_number", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
