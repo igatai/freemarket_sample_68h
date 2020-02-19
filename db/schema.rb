62f8679895023b06dd027d13a76635efe72285bf
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-19 12:53:37 +0900

    create-brands-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_053621) do
+ActiveRecord::Schema.define(version: 2020_02_19_032021) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image", null: false
