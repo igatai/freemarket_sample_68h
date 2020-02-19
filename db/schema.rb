f3d27b24e75e590bef64cfe22b1208c738394e15
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-02-19 17:28:35 +0900

    edit detail point
    枠線の色変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_144748) do
+ActiveRecord::Schema.define(version: 2020_02_19_035635) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zipcode"
@@ -23,8 +23,13 @@ ActiveRecord::Schema.define(version: 2020_02_18_144748) do
     t.integer "phone_number"
     t.integer "prefecture_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
-ActiveRecord::Schema.define(version: 2020_02_19_035635) do
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -32,12 +37,6 @@ ActiveRecord::Schema.define(version: 2020_02_19_035635) do
     t.datetime "updated_at", null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
-ActiveRecord::Schema.define(version: 2020_02_19_032021) do
-
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
