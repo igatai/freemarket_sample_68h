91c920e6a7e4d09594e8782f1e3751bf9be089fe
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-19 16:20:03 +0900

    product-index-image-update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,25 @@
 class ProductsController < ApplicationController
 
   def index
-    @product = Product.all.limit(3)
+    @product = Product.where(brand_id: "1").first(3)
+    @brand = Product.where(brand_id: "2").first(3)
   end
 
   def new
 
   end
 
+  def create
+    @product = Product.new(product_params)
+  end
+
   def show
 
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :content, :image_id)
+  end  
 end
