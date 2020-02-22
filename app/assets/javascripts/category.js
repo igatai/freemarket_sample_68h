2a4a8bf791db17e6d20aa91118cfc7bac5693776
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-02-22 20:04:01 +0900

    get element chirdlen category
    子クラスのカテゴリ取得後、表示機能の実装完了

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,4 +1,25 @@
 $(function(){
+
+  function appendCategory(child){
+    var html = `
+                <option value = "${child.name}" data-category="${child.id}">${child.name}</option>
+              `;
+    return html;
+    
+  }
+  function appendchildrenbox(insertHTML){
+    var secondhtml = 
+              `
+                  <div class = "contents__detail__box__set__form__detail">
+                    <select class = "contents__detail__box__set__form", id = "children_category">
+                      <option value="---">---</option>
+                      ${insertHTML}
+                    </select>
+                  </div>
+              `;
+    $('.contents__detail__box__set__form__detail').append(secondhtml);
+  }
+
   $("#parent_category").on("change", function(){
     var parentCategory = document.getElementById('parent_category').value;
     console.log(parentCategory);
@@ -9,6 +30,42 @@ $(function(){
         data: { parent_name: parentCategory },
         dataType: 'json'
       })
-    }
+      .done(function(children){
+        var insertHTML = '';
+        
+        children.forEach(function(child){
+          insertHTML += appendCategory(child);
+        });
+        appendchildrenbox(insertHTML);
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    };
+  });
+  
+  $("contents__detail__box__set__form").on("change", function(){
+    console.log("発火");
+    // var parentCategory = document.getElementById('parent_category').value;
+    // console.log(parentCategory);
+    // if (parentCategory != "---"){ 
+    //   $.ajax({
+    //     url: 'get_category_children',
+    //     type: 'GET',
+    //     data: { parent_name: parentCategory },
+    //     dataType: 'json'
+    //   })
+    //   .done(function(children){
+    //     var insertHTML = '';
+        
+    //     children.forEach(function(child){
+    //       insertHTML += appendCategory(child);
+    //     });
+    //     appendchildrenbox(insertHTML);
+    //   })
+    //   .fail(function(){
+    //     alert('カテゴリー取得に失敗しました');
+    //   })
+    // };
   })
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,12 +14,18 @@ class ProductsController < ApplicationController
     #親カテゴリーのみ抽出 => 配列
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
-
-
     end
     # binding.pry
   end
 
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
 
 
 
