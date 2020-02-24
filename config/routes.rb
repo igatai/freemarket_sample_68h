0408c3f10ad738a40652677a589bf84a8bfb88e6
MasahiroTagashira <tagashira0624@gmail.com>
2020-02-24 12:06:05 +0900

    fixed_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  # devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root 'products#index'
+
+  resources :users, only: [:new, :create, :show]
+  resources :registration, only: [:index, :new, :create]
+
   resources :products, only: [:index, :new, :create, :show]
   resources :useraddress, only: [:index]
   resources :purchase, only: :new
-  resources :credit, only: :new
+  resources :credit, only: [:index, :new, :create]
   resources :useraddress, only: :new
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
