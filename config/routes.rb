58170c2b60c0e43c1cf294cc3aca3554046d3673
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-24 10:48:28 +0900

    products-show-view-update

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :show]
   resources :registration, only: [:index, :new, :create]
 
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
   resources :useraddress, only: [:index]
   resources :purchase, only: :new
   resources :credit, only: :new
