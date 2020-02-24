ca1fe75dfe3000df689671709245c3699b1037ad
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-24 18:46:36 +0900

    product-delete-update

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  has_many :images dependent: :destroy
+  has_many :images, dependent: :destroy
   belongs_to :category
   belongs_to :brand
   belongs_to :user
