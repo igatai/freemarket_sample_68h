39d8094f5643c0f0e0751eddb045cb28afe3b7cd
MasahiroTagashira <tagashira0624@gmail.com>
2020-02-24 12:09:27 +0900

    fixed_schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,86 @@
-Rails.application.routes.draw do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-  }
-  devise_scope :user do
-    get 'addresses', to: 'users/registrations#new_address'
-    post 'addresses', to: 'users/registrations#create_address'
+ActiveRecord::Schema.define(version: 2020_02_19_101031) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "prefecture"
+    t.integer "zipcode"
+    t.text "city"
+    t.text "address"
+    t.text "building"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "phone_number"
+    t.integer "prefecture_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-  root 'products#index'
 
-  resources :users, only: [:new, :create, :show]
-  resources :registration, only: [:index, :new, :create]
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
-  resources :products, only: [:index, :new, :create, :show]
-  resources :useraddress, only: [:index]
-  resources :purchase, only: :new
-  resources :credit, only: [:index, :new, :create]
-  resources :useraddress, only: :new
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.integer "condition", null: false
+    t.string "status", null: false
+    t.string "payment", null: false
+    t.integer "delivery_date", null: false
+    t.string "delivery_method", null: false
+    t.integer "price", null: false
+    t.integer "user_id", null: false
+    t.integer "brand_id", null: false
+    t.integer "category_id", null: false
+    t.integer "prefecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "birthyear", null: false
+    t.string "birthmonth", null: false
+    t.string "birthday", null: false
+    t.string "family_name_zenkaku", null: false
+    t.string "first_name_zenkaku", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "images", "products"
 end
