6136a17445cf9aec3ef2bbe8836411b68fd6739c
igatai <taiti0614@gmail.com>
2020-02-24 21:10:53 +0900

    commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
