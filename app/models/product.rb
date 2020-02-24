9de5f9244c06126cca611340c30cb99531b22a55
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-24 21:04:30 +0900

    product-show-update

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
   belongs_to :brand
