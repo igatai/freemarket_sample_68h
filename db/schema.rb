cd4c5b082c7fc63b1b6b9e567c6848d9f4561e07
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-02-25 16:17:56 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,21 @@
 ActiveRecord::Schema.define(version: 2020_02_25_040813) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id", null: false
-    t.integer "zipcode", null: false
-    t.text "city", null: false
-    t.text "address", null: false
+    t.text "prefecture"
+    t.integer "zipcode"
+    t.text "city"
+    t.text "address"
     t.text "building"
-    t.text "phone_number"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "phone_number"
+    t.integer "prefecture_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "name", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -97,7 +98,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_040813) do
   end
 
   add_foreign_key "cards", "users"
-  add_foreign_key "addresses", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "brands"
 end
