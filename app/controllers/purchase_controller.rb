165526216c16900e9478105633f23d066d0d6ccc
koushi-0722 <kushi.ooyama@gmaill.com>
2020-02-27 11:13:48 +0900

    commentout-product-shows

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,11 +1,11 @@
 class PurchaseController < ApplicationController
 
-  # def show
-  #   @product = Product.find(params[:id])
-  #   @user = User.find(params[:id])
-  #   if user_signed_in? && current_user.id == @product.user_id
-  #     redirect_to "/products/#{@product.id}"
-  #   end
-  # end
+  def show
+    @product = Product.find(params[:id])
+    @user = User.find(params[:id])
+    if user_signed_in? && current_user.id == @product.user_id
+      redirect_to "/products/#{@product.id}"
+    end
+  end
 
 end
