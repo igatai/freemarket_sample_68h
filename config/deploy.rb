31c97679536c54a22d5e72b810a79d3a0879972f
MasahiroTagashira <tagashira0624@gmail.com>
2020-02-27 21:32:07 +0900

    add_s3_upload_conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,4 +32,22 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
+
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: Rails.application.credentials.aws[:access_key_id],
+#   AWS_SECRET_ACCESS_KEY: Rails.application.credentials.aws[:secret_access_key]
+}
