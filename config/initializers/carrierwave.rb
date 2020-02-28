76c3ff5d4b36aca0ba29f8823dc95d44ca9036a9
MasahiroTagashira <tagashira0624@gmail.com>
2020-02-28 15:27:03 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1' # 東京はap-northeast-1  ❶
     }
     config.fog_directory  = 'taiti0614' #S3のバケット名❷
-    config.asset_host = 'https://s3-your_region.amazonaws.com/taiti0614'  # ③
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/taiti0614'  # ③
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
