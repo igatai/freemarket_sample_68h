227d7c72e516adc4fef31b6cb1dfa2c47052ce62
Masaru-0816 <two-of-us.0816@hotmail.co.jp>
2020-03-01 07:19:00 +0900

    changed purchase controller

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -5,16 +5,16 @@ class PurchaseController < ApplicationController
   
   def show
     # 登録カード情報
-    if card.blank?
+    if @card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to cards_path
       flash[:alert] = '購入にはクレジットカード登録が必要です'
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
 
     # 購入者住所情報
@@ -32,7 +32,7 @@ class PurchaseController < ApplicationController
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       Payjp::Charge.create(
       amount: @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-      customer: card.customer_id, #顧客ID
+      customer: @card.customer_id, #顧客ID
       currency: 'jpy', #日本円
       )
       redirect_to action: 'done' #完了画面に移動
@@ -48,6 +48,6 @@ class PurchaseController < ApplicationController
   end
 
   def set_card
-    card = Card.where(user_id: current_user.id).first
+    @card = Card.where(user_id: current_user.id).first
   end
 end
