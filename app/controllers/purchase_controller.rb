1f8c30e83bd65348aae95d9ffc94aedd1ff0d186
igatai <taiti0614@gmail.com>
2020-03-02 13:22:35 +0900

    commit

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,7 +2,8 @@ class PurchaseController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     if user_signed_in? && current_user.id == @product.user_id
       redirect_to "/products/#{@product.id}"
     end
